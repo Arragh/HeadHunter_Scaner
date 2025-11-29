d8b9142f915456e9de80fa3c64219247b73e5b5a
Sergei <arragh1986@icloud.com>
2025-11-29 20:27:58 +0300

    feat: тест для TestParseVacanciesIds

--- service/headhunter/hh_service_test.go
+++ service/headhunter/hh_service_test.go
@@ -16,3 +16,21 @@ func TestDifference(t *testing.T) {
 		t.Errorf("Difference() = \"%v\", want %v", got, want)
 	}
 }
+
+func TestParseVacanciesIds(t *testing.T) {
+	vacancies := []Vacancy{
+		{Id: "123"},
+		{Id: "456"},
+		{Id: "789"},
+	}
+
+	got, err := ParseVacanciesIds(vacancies)
+	if err != nil {
+		t.Errorf("ошибка при парсинге вакансий: %v", err)
+	}
+
+	want := []int64{123, 456, 789}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+}
