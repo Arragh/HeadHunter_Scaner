79bd0dbd8d50de5dafeb33f0b68d7e79002ca83f
Sergei <arragh1986@icloud.com>
2025-11-30 01:15:42 +0300

    feat: дополнительные тесты для десериализатора

--- service/serializer/serializer_test.go
+++ service/serializer/serializer_test.go
@@ -6,12 +6,8 @@ import (
 	"testing"
 )
 
-func TestDeserialize(t *testing.T) {
-	testBody := TestStruct{
-		Field: "test",
-	}
-
-	serializedBody, err := json.Marshal(testBody)
+func TestDeserialize_Valid(t *testing.T) {
+	serializedBody, err := json.Marshal(TestStruct{Field: "test"})
 	if err != nil {
 		t.Errorf("ошибка сериализации тестового body %v", err)
 	}
@@ -25,14 +21,33 @@ func TestDeserialize(t *testing.T) {
 		t.Fatal("deserialized = nil")
 	}
 
-	want := TestStruct{
-		Field: "test",
-	}
+	want := TestStruct{Field: "test"}
 
 	got := *deserialized
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("Deserialize() = %v, want %v", got, want)
+	}
+}
+
+func TestDeserialize_InvalidType(t *testing.T) {
+	_, err := Deserialize[TestStruct]([]byte("12345"))
+	if err == nil {
+		t.Error("ожидалась ошибка десериализации из-за неверного типа")
+	}
+}
+
+func TestDeserialize_InvalidBody(t *testing.T) {
+	_, err := Deserialize[TestStruct]([]byte("{Field: \"test\""))
+	if err == nil {
+		t.Fatal("ожидалась ошибка десериализации из-за битого body")
+	}
+}
+
+func TestDeserialize_NilBody(t *testing.T) {
+	_, err := Deserialize[TestStruct](nil)
+	if err == nil {
+		t.Error("ожидалась ошибка десериализации из-за nil-body")
 	}
 }
 
